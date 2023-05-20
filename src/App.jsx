164e9c3c54721a1c704c3062b5ffc9733351eb14
MaesterGray <historycurators@gmail.com>
2023-05-20 03:55:58 +0100

    ipad and desktop views

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './Pages/login'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
+    <Routes>
+      <Route />
     <Login/>
-    </>
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
